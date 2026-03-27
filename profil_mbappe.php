af8e86ff66f60e4311f1c2060ff761ed76b306aa
qays chaabaoui <qayschaabaoui@gmail.com>
2026-03-27 15:46:20 +0100

    Refactor profil_mbappe.php to use header and footer includes

--- profil_mbappe.php
+++ profil_mbappe.php
@@ -9,22 +9,7 @@
 </head>
 
 <body>
-
-    <header>
-        <div class="logo">
-            <h1>FC BURGER DREUX ⚽</h1>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="index.html">Le Stade (Accueil)</a></li>
-                <li><a href="menu.html">La Compo (Carte)</a></li>
-                <li><a href="connexion.html">Vestiaires (Connexion)</a></li>
-                <li><a href="panier.html">Mon Panier</a></li>
-                <li><a href="admin.html">Bureau du Coach</a></li>
-                <li><a href="notation.html">Avis</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php require_once('includes/header.php'); ?>
 
     <main>
         <section class="profile-section">
@@ -95,10 +80,9 @@
         </section>
     </main>
 
-    <footer>
-        <p>&copy; 2025 FC Burger Dreux</p>
-    </footer>
+    <?php require_once('includes/footer.php'); ?>
 
 </body>
 
 </html>
+
