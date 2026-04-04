96250356acbd125b5d0f61fe4f7537dfee6ac9d4
qays chaabaoui <qayschaabaoui@gmail.com>
2026-04-05 00:31:21 +0200

    Enhance header with user role links and feedback option
    Added 'Avis' link for user feedback and improved admin role handling.

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -12,11 +12,15 @@ if (session_status() === PHP_SESSION_NONE) {
             <li><a href="index.php">Le Stade</a></li>
             <li><a href="menu.php">La Compo</a></li>
             <li><a href="panier.php">Mon Panier</a></li>
-            
-            <?php 
-            if(isset($_SESSION['user_nom'])): ?>
-                <li><a href="profil.php" style="color: var(--jaune-or);">Licence de <?php echo $_SESSION['user_nom']; ?></a></li>
-                <li><a href="logout.php">Déconnexion</a></li>
+            <li><a href="notation.php">Avis</a></li>
+            <?php
+            if (isset($_SESSION['user_nom'])): ?>
+                <li><a href="profil.php" style="color: var(--jaune-or);">Licence de <?php echo $_SESSION['user_nom']; ?></a>
+                </li>
+                <?php if ($_SESSION['user_role'] === 'admin'): ?>
+                    <li><a href="admin.php" style="color: #2ecc71;">Coach (Admin)</a></li>
+                <?php endif; ?>
+                <li><a href="deconnexion.php" style="color: #ff4757;">Quitter le terrain</a></li>
             <?php else: ?>
                 <li><a href="connexion.php">Vestiaires (Connexion)</a></li>
             <?php endif; ?>
