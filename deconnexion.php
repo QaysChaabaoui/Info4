ad775958b69a934344352e380c665e416bca9c42
qays chaabaoui <qayschaabaoui@gmail.com>
2026-04-05 16:27:04 +0200

    Fix session handling in deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,7 +1,7 @@
 <?php
-session_start(); // OBLIGATOIRE : on doit ouvrir la session pour pouvoir la fermer
-session_unset(); // On vide les variables
-session_destroy(); // On détruit le fichier de session sur le serveur
-header("Location: index.php"); // On renvoie vers l'accueil
+session_start();
+session_unset(); 
+session_destroy(); 
+header("Location: index.php"); 
 exit();
 ?>
