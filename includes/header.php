93885359aa247a8a97e983631c5ecec8ddbd55b2
qays chaabaoui <qayschaabaoui@gmail.com>
2026-04-05 16:25:53 +0200

    Refactor navigation links and classes in header

--- includes/header.php
+++ includes/header.php
@@ -13,14 +13,22 @@ if (session_status() === PHP_SESSION_NONE) {
             <li><a href="menu.php">La Compo</a></li>
             <li><a href="panier.php">Mon Panier</a></li>
             <li><a href="notation.php">Avis</a></li>
-            <?php
-            if (isset($_SESSION['user_nom'])): ?>
-                <li><a href="profil.php" style="color: var(--jaune-or);">Licence de <?php echo $_SESSION['user_nom']; ?></a>
-                </li>
+            <?php if (isset($_SESSION['user_nom'])): ?>
+                <li><a href="profil.php" class="nav-profile">Licence de <?php echo $_SESSION['user_nom']; ?></a></li>
+                
                 <?php if ($_SESSION['user_role'] === 'admin'): ?>
-                    <li><a href="admin.php" style="color: #2ecc71;">Coach (Admin)</a></li>
+                    <li><a href="admin.php" class="nav-admin">Coach (Admin)</a></li>
                 <?php endif; ?>
-                <li><a href="deconnexion.php" style="color: #ff4757;">Quitter le terrain</a></li>
+                
+                <?php if ($_SESSION['user_role'] === 'restaurateur' || $_SESSION['user_role'] === 'admin'): ?>
+                    <li><a href="restaurateur.php" class="nav-kitchen">👨‍🍳 Ma Cuisine</a></li>
+                <?php endif; ?>
+                
+                <?php if (isset($_SESSION['user_role']) && ($_SESSION['user_role'] === 'livreur' || $_SESSION['user_role'] === 'admin')): ?>
+                    <li><a href="livreur.php" class="nav-delivery">🚲 Livraisons</a></li>
+                <?php endif; ?>
+                
+                <li><a href="deconnexion.php" class="nav-logout">Quitter le terrain</a></li>
             <?php else: ?>
                 <li><a href="connexion.php">Vestiaires (Connexion)</a></li>
             <?php endif; ?>
