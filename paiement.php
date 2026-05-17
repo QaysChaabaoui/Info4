df427158fd2d3f3501f3cf98a75e3e03e0722459
aymvn95 <aymanmadrassi@gmail.com>
2026-05-17 16:32:42 +0200

    Update paiement.php

--- paiement.php
+++ paiement.php
@@ -12,6 +12,20 @@ if (isset($_SESSION['panier']) && !empty($_SESSION['panier'])) {
     }
 }
 
+$est_une_modification = isset($_SESSION['modification_commande_id']);
+$montant_deja_paye = $est_une_modification ? $_SESSION['montant_deja_paye'] : 0.0;
+
+$montant_a_payer_banque = $total_a_payer;
+if ($est_une_modification) {
+    $montant_a_payer_banque = $total_a_payer - $montant_deja_paye;
+}
+
+if ($montant_a_payer_banque <= 0 && $est_une_modification) {
+    header("Location: confirmation.php?status=accepted&transaction=MOD-INTERNE&montant=0.00&control=" . md5($api_key . "#MOD-INTERNE#0.00#" . $vendeur . "#accepted#"));
+    exit();
+}
+
+$total_a_payer = $montant_a_payer_banque;
 $montant_formatte = number_format($total_a_payer, 2, '.', '');
 $transaction = "FCB" . substr(md5(uniqid(mt_rand(), true)), 0, 12);
 // Détection automatique de ton adresse pour éviter l'erreur ERR_CONNECTION_REFUSED
@@ -20,6 +34,9 @@ $host = $_SERVER['HTTP_HOST'];
 $path = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 
 $retour = $protocol . "://" . $host . $path . "/confirmation.php";
+if (isset($est_une_modification) && $est_une_modification) {
+    $retour .= "?mod_id=" . $_SESSION['modification_commande_id'] . "&total_global=" . number_format($total_a_payer + $montant_deja_paye, 2, '.', '');
+}
 
 $chaine_a_hacher = $api_key . "#" . $transaction . "#" . $montant_formatte . "#" . $vendeur . "#" . $retour . "#";
 $control = md5($chaine_a_hacher);
